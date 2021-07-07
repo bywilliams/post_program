9f419142bbcafb0db3de891bd9af65c6197a7f06
bywilliams <willian.seu@gmail.com>
2021-07-07 19:10:28 -0300

    Post Program Project

---
+++ src/applications/Program.java
@@ -0,0 +1,48 @@
+package applications;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import entities.Comment;
+import entities.Post;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		
+		// definindo os comnetarios dos primeiro post
+		Comment c1 = new Comment("Have a nice trip");
+		Comment c2 = new Comment("Wow that's awesome!");
+		
+		// criando o objeto post e instanciando 
+		Post p1 = new Post(
+				sdf.parse("21/06/2018 13:05:44"), 
+				"Traveling to New Zealand", 
+				"I'm going to visit this wonderful country!", 
+				12);
+		// chamando o metodo de add comnetario da classe post passando como paramento o coment c1 e c2
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		// definindo comentarios do 2 post
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+		
+		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"), 
+				"Good night guys", 
+				"See you tomorrow", 
+				5);
+		
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+		
+	}
+
+}
